54a28440d42bb96740f94d6bbbfc544db6a19107
rama <sriramkidambi1997@gmail.com>
2023-03-30 00:34:27 +0530

    fixes

--- arch/software-install.sh
+++
@@ -1,9 +0,0 @@
-sudo pacman -Sy
-sudo pacman -S --needed git base-devel
-git clone  https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
-
-yay -S google-chrome brave-bin discord ferdium-bin bitwarden-desktop guilded
-
-
