645ce75b6b951d3fd9df5ab408e338bc27de1a77
0xrama <rama@0xrama.xyz>
2023-12-17 07:04:12 +0530

    Delete unused files and update KVM installation script

--- debian/qemu-setup.sh
+++ debian/qemu-setup.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-# Display ASCII art
-echo "         _nnnn_                       "
-echo "        dGGGGMMb     ,\"\"\"\"\"\"\"\"\"\"\"."
-echo "       @p~qp~~qMb    | Installing QEMU |"
-echo "       M|@||@) M|   _;..............'"
-echo "       @,----.JM| -'"
-echo "      JS^\__/  qKL"
-echo "     dZP        qKRb"
-echo "    dZP          qKKb"
-echo "   fZP            SMMb"
-echo "   HZM            MMMM"
-echo "   FqM            MMMM"
-echo " __| \".        |\\dS\"qML"
-echo " |    \`.       | \`' \\Zq"
-echo "_)      \\.___.,|     .'"
-echo "\\____   )MMMMMM|   .'"
-echo "     \`-'       \`--' hjm"
-sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager -y
-sudo virsh net-start default
-sudo virsh net-autostart default
-sudo usermod -aG libvirt $USER
-sudo usermod -aG libvirt-qemu $USER
-sudo usermod -aG kvm $USER
-sudo usermod -aG input $USER
-sudo usermod -aG disk $USER
+# Hide commands being run
+set -e
+set +x
+
+# Display ASCII penguin with waiting sign
+whiptail --title "KVM Installation" --infobox "Installing KVM, please wait..." 10 50
+
+# Install KVM packages
+apt-get update
+apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virtinst
+
+# Enable and start libvirtd service
+systemctl enable libvirtd
+systemctl start libvirtd
+
+# Add user to the libvirt group
+usermod -aG libvirt $(whoami)
+
+# Display completion message
+whiptail --title "KVM Installation" --msgbox "KVM installation and configuration completed successfully!" 10 50
